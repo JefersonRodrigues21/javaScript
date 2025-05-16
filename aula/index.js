6aba7bc8f314629b57bbc991f3ecd420b1614e31
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-05-16 15:49:54 -0300

    inicio var e console.log

--- aula/index.js
+++ aula/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!!!')
+//Utilize sempre o Camel Case no JavaScript!
+
+//JavaScript possui tipagem dinâmica: infere os tipos de dados
+//JavaScript possui uma tipagem fraca
+
+var nomeDoProfessor = 'Walisson Silva' //string (textos) utiliza sempre aspas
+var idade = 27 //number
+var altura = 1.77 //number
+var estudando = true //boolean (booleano: verdadeiro ou falso)
+
+console.log(nomeDoProfessor, typeof nomeDoProfessor)
+console.log(idade, typeof idade)
+console.log(altura, typeof altura)
+console.log(estudando, typeof estudando)
