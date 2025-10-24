1be10b58f8ea66515432c92d28ccfe5a84f9ff44
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-10-24 17:47:39 -0300

    operadores boleanos

---
+++ aula-07/operadores-booleanos.js
@@ -0,0 +1,8 @@
+// Operadores Booleanos: Comparações.
+
+const numero1 = 10;
+const numero2 = 12;
+
+console.log(numero1 == numero2); // Igualdade: verifica se os valores são iguais (retorna true ou false)
+console.log(numero1 === numero2); // Estrita Igualdade: verifica se os valores e tipos são iguais (retorna true ou false)
+console.log(numero1 === "10"); // false
