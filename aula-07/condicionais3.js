a93a55f114837da8285e3a575aca126d17a6a5ec
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-10-26 23:04:29 -0300

    finish

---
+++ aula-07/condicionais3.js
@@ -0,0 +1,9 @@
+// * Operador Ternário* - Uma forma mais curta de escrever condicionais simples
+
+const idade = 15;
+
+// idade >= 18 ? console.log('Maior de idade') : console.log('Menor de idade');
+
+const mensagem = idade >= 18 ? 'Maior de idade' : 'Menor de idade'; // ? = if, e : = else
+
+console.log(mensagem);
